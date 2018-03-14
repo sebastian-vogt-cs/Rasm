9db34711218946dfba51ef29a865d478f2fa7a8b
Sebastian <vogt999@gmail.com>
2018-03-14 19:03:24 +0100

    It works! Associate integer with each Command in enum. Move new() into RegisterMachine implementation (was in mod register_machine before). Make example that actually works (with jumps). I indexed the commands from 1 to max because that's the way our school-interpreter does it, but I wonder if we shouldn't internally index it from 0 to max.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
 mod register_machine;
+use register_machine::RegisterMachine;
 use register_machine::Command;
 
 fn main() {
-    let mut machine = register_machine::new();
-    machine.run(Command::DLOAD, 100);
-    machine.run(Command::STORE, 1);
-    machine.run(Command::DLOAD, 1);
-    machine.run(Command::STORE, 2);
-    machine.run(Command::DLOAD, 0);
-    machine.run(Command::STORE, 3);
-    machine.run(Command::LOAD, 3);
-    machine.run(Command::ADD, 1);
-    machine.run(Command::STORE, 3);
-    machine.run(Command::LOAD, 1);
-    machine.run(Command::SUB, 2);
-    machine.run(Command::STORE, 1);
-    machine.run(Command::JGT, 7);
-    machine.run(Command::LOAD, 3);
-    machine.run(Command::END, 0);
+    let commands = [Command::DLOAD(10), Command::STORE(1), Command::DLOAD(0), Command::STORE(2), Command::DLOAD(1), Command::STORE(3), Command::DLOAD(0),
+                    Command::STORE(4), Command::DLOAD(1), Command::STORE(5), Command::SUB(1), Command::JGT(23), Command::LOAD(3), Command::STORE(4),
+                    Command::LOAD(2), Command::STORE(3), Command::ADD(4), Command::STORE(2), Command::DLOAD(1), Command::ADD(5), Command::STORE(5),
+                    Command::JUMP(11), Command::LOAD(2), Command::END];
+
+    let mut machine = RegisterMachine::new();
+    loop{
+        let counter = commands[machine.get_instruction_counter() - 1];
+        machine.run(counter);
+        if counter == Command::END{
+            break;
+        }
+    }
+
 }
--- src/register_machine.rs
+++ src/register_machine.rs
@@ -1,18 +1,19 @@
+#[derive(Copy, Clone, PartialEq)]
 pub enum Command {
-    LOAD,
-    DLOAD,
-    STORE,
-    ADD,
-    SUB,
-    MULT,
-    DIV,
-    JUMP,
-    JGE,
-    JGT,
-    JLE,
-    JLT,
-    JEQ,
-    JNE,
+    LOAD(u8),
+    DLOAD(isize),
+    STORE(u8),
+    ADD(u8),
+    SUB(u8),
+    MULT(u8),
+    DIV(u8),
+    JUMP(usize),
+    JGE(usize),
+    JGT(usize),
+    JLE(usize),
+    JLT(usize),
+    JEQ(usize),
+    JNE(usize),
     END,
 }
 
@@ -28,18 +29,6 @@ pub struct RegisterMachine {
     accumulator: Register,
     instuction_counter: Register,
     data_registers: [Register; 16],
-}
-
-pub fn new() -> RegisterMachine {
-    RegisterMachine {
-        accumulator: Register{
-            value: 0,
-        },
-        instuction_counter: Register {
-            value: 0,
-        },
-        data_registers: [Register{value: 0,}; 16],
-    }
 }
 
 
@@ -66,28 +55,44 @@ impl Register {
 
 impl RegisterMachine{
 
-    pub fn run(&mut self, command: Command, n: isize){
-        match command{
-            Command::LOAD   => self.load(n),
-            Command::DLOAD  => self.d_load(n),
-            Command::STORE  => self.store(n),
-            Command::ADD    => self.add(n),
-            Command::SUB    => self.sub(n),
-            Command::MULT   => self.mult(n),
-            Command::DIV    => self.div(n),
-            Command::JUMP   => self.jump(n),
-            Command::JGE    => self.jge(n),
-            Command::JGT    => self.jgt(n),
-            Command::JLE    => self.jle(n),
-            Command::JLT    => self.jlt(n),
-            Command::JEQ    => self.jeq(n),
-            Command::JNE    => self.jne(n),
-            Command::END    => self.end(),
+    pub fn new() -> RegisterMachine {
+        RegisterMachine {
+            accumulator: Register{
+                value: 0,
+            },
+            instuction_counter: Register {
+                value: 1,
+            },
+            data_registers: [Register{value: 0,}; 16],
         }
     }
 
-    fn load(&mut self, n: isize) {
-        if (n > 15) && (n < 0) {
+    pub fn run(&mut self, command: Command){
+        match command{
+            Command::LOAD(i)   => self.load(i),
+            Command::DLOAD(i)  => self.d_load(i),
+            Command::STORE(i)  => self.store(i),
+            Command::ADD(i)    => self.add(i),
+            Command::SUB(i)    => self.sub(i),
+            Command::MULT(i)   => self.mult(i),
+            Command::DIV(i)    => self.div(i),
+            Command::JUMP(i)   => self.jump(i),
+            Command::JGE(i)    => self.jge(i),
+            Command::JGT(i)    => self.jgt(i),
+            Command::JLE(i)    => self.jle(i),
+            Command::JLT(i)    => self.jlt(i),
+            Command::JEQ(i)    => self.jeq(i),
+            Command::JNE(i)    => self.jne(i),
+            Command::END       => self.end(),
+        }
+    }
+
+    pub fn get_instruction_counter(&self) -> usize{
+        self.instuction_counter.value as usize
+    }
+
+    fn load(&mut self, n: u8) {
+        if n > 15 {
             panic!(format!("Register {} does not exist.", n));
         }
         self.instuction_counter.add(1);
@@ -99,93 +104,93 @@ impl RegisterMachine{
         self.accumulator.value = n;
     }
 
-    fn store(&mut self, n: isize) {
-        if (n > 15) && (n < 0) {
+    fn store(&mut self, n: u8) {
+        if n > 15 {
             panic!(format!("Register {} does not exist.", n));
         }
         self.instuction_counter.add(1);
         self.data_registers[n as usize].value = self.accumulator.value;
     }
 
-    fn add(&mut self, n: isize) {
-        if (n > 15) && (n < 0) {
+    fn add(&mut self, n: u8) {
+        if n > 15 {
             panic!(format!("Register {} does not exist.", n));
         }
         self.instuction_counter.add(1);
         self.accumulator.add(self.data_registers[n as usize].value);
     }
 
-    fn sub(&mut self, n: isize) {
-        if (n > 15) && (n < 0) {
+    fn sub(&mut self, n: u8) {
+        if n > 15 {
             panic!(format!("Register {} does not exist.", n));
         }
         self.instuction_counter.add(1);
         self.accumulator.sub(self.data_registers[n as usize].value);
     }
 
-    fn mult(&mut self, n: isize) {
-        if (n > 15) && (n < 0) {
+    fn mult(&mut self, n: u8) {
+        if n > 15 {
             panic!(format!("Register {} does not exist.", n));
         }
         self.instuction_counter.add(1);
         self.accumulator.mult(self.data_registers[n as usize].value);
     }
 
-    fn div(&mut self, n: isize) {
-        if (n > 15) && (n < 0) {
+    fn div(&mut self, n: u8) {
+        if n > 15 {
             panic!(format!("Register {} does not exist.", n));
         }
         self.instuction_counter.add(1);
         self.accumulator.div(self.data_registers[n as usize].value);
     }
 
-    fn jump(&mut self, n: isize) {
-        self.instuction_counter.value = n;
+    fn jump(&mut self, n: usize) {
+        self.instuction_counter.value = n as isize;
     }
 
-    fn jge(&mut self, n: isize) {
+    fn jge(&mut self, n: usize) {
         if self.accumulator.value >= 0{
-            self.instuction_counter.value = n;
+            self.instuction_counter.value = n as isize;
         }else{
             self.instuction_counter.add(1);
         }
     }
 
-    fn jgt(&mut self, n: isize) {
+    fn jgt(&mut self, n: usize) {
         if self.accumulator.value > 0{
-            self.instuction_counter.value = n;
+            self.instuction_counter.value = n as isize;
         }else{
             self.instuction_counter.add(1);
         }
     }
 
-    fn jle(&mut self, n: isize) {
+    fn jle(&mut self, n: usize) {
         if self.accumulator.value <= 0{
-            self.instuction_counter.value = n;
+            self.instuction_counter.value = n as isize;
         }else{
             self.instuction_counter.add(1);
         }
     }
 
-    fn jlt(&mut self, n: isize) {
+    fn jlt(&mut self, n: usize) {
         if self.accumulator.value < 0{
-            self.instuction_counter.value = n;
+            self.instuction_counter.value = n as isize;
         }else{
             self.instuction_counter.add(1);
         }
     }
 
-    fn jeq(&mut self, n: isize) {
+    fn jeq(&mut self, n: usize) {
         if self.accumulator.value == 0{
-            self.instuction_counter.value = n;
+            self.instuction_counter.value = n as isize;
         }else{
             self.instuction_counter.add(1);
         }
     }
 
-    fn jne(&mut self, n: isize) {
+    fn jne(&mut self, n: usize) {
         if self.accumulator.value != 0{
-            self.instuction_counter.value = n;
+            self.instuction_counter.value = n as isize;
         }else{
             self.instuction_counter.add(1);
         }
@@ -194,7 +199,7 @@ impl RegisterMachine{
     fn end(&mut self) {
         println!("accumulator: {}", self.accumulator.value);
         self.accumulator.value = 0;
-        self.instuction_counter.value = 0;
+        self.instuction_counter.value = 1;
         for i in 0..self.data_registers.len(){
             self.data_registers[i].value = 0;
         }
